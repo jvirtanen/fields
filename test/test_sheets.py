1c0608ea54b10cecffc261bbc653d507a1b565c8
Jussi Virtanen <jussi.k.virtanen@gmail.com>
2012-06-08 00:19:03 +0300

    Remove 'support' module

--- test/test_sheets.py
+++ test/test_sheets.py
@@ -1,6 +1,29 @@
 #!/usr/bin/env python
 
-from support import TestCase, main
+import dump
+import unittest
+
+
+class TestCase(unittest.TestCase):
+
+    SHEETS_ERROR_TOO_BIG_RECORD = 1
+    SHEETS_ERROR_TOO_MANY_FIELDS = 2
+    SHEETS_ERROR_UNEXPECTED_CHARACTER = 3
+    SHEETS_ERROR_UNREADABLE_SOURCE = 4
+
+    def assert_equals(self, text, table):
+        self.__assert_equals(text, dump.dump_table(table), b=False)
+        self.__assert_equals(text, dump.dump_table(table), b=True)
+
+    def assert_error(self, text, message):
+        self.__assert_equals(text, message, b=False)
+        self.__assert_equals(text, message, b=True)
+
+    def __assert_equals(self, text, expected, b):
+        self.assertEquals(self.__dump_text(text, b), expected)
+
+    def __dump_text(self, text, b):
+        return dump.dump_text(text, b, self.d, self.e, self.q)
 
 
 class CSVTest(TestCase):
@@ -126,4 +149,4 @@ class TSVTest(TestCase):
 
 
 if __name__ == "__main__":
-    main()
+    unittest.main()
