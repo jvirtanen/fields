1937b86bff199d047b90530500e8a93229c04588
Jussi Virtanen <jussi.k.virtanen@gmail.com>
2012-08-11 20:37:53 +0300

    Fill setup script

--- python/setup.py
+++ python/setup.py
@@ -4,5 +4,12 @@ from distutils.core import setup
 
 setup(
     name='fields',
-    version='0.4.0'
+    version='0.4.0',
+    description='Python binding to Fields, ' \
+        + 'the fast C library for reading CSV and other tabular text formats',
+    url='https://github.com/jvirtanen/fields',
+    license='MIT',
+    author='Jussi Virtanen',
+    author_email='jussi.k.virtanen@gmail.com',
+    packages=['fields']
 )
