381094b885658e0a89429bde42a9dd80a9ce5c4d
Jussi Virtanen <jussi.k.virtanen@gmail.com>
2012-08-11 12:21:32 +0300

    Document 'fields' module

--- python/fields/__init__.py
+++ python/fields/__init__.py
@@ -1,2 +1,7 @@
+"""
+This module contains a Python binding to Fields, the fast C library for
+reading CSV and other tabular text formats.
+"""
+
 from .csv import Error
 from .csv import Reader as reader
