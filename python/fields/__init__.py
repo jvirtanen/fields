ee4187295c654e1dfae55d687c0b79d1d38385d5
Jussi Virtanen <jussi.k.virtanen@gmail.com>
2013-02-05 23:49:51 +0200

    Rename 'fields.csv' to 'fields.api'

--- python/fields/__init__.py
+++ python/fields/__init__.py
@@ -3,4 +3,4 @@ This module contains a Python binding to Fields, the fast C library for
 reading CSV and other tabular text formats.
 """
 
-from .csv import Error, reader
+from .api import Error, reader
